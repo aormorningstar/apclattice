43fbdf7bcde0e30a862206b21b9a3b7f6ef7e2fe
lunlgit <lunl@princeton.edu>
2020-11-16 10:06:13 -0500

    Tried to implement HoneycombUnitCell

--- src/unitcell/hcunitcell.py
+++ src/unitcell/hcunitcell.py
@@ -1,16 +1,26 @@
-# updated: 11-15-2020
+# updated: 11-16-2020
 
 import numpy as np
-import unitcell
+from .unitcell import UnitCell
 
-class UnitCellParam:
-    def __init__(self, cell_scaling):
-        self.cell_scaling = cell_scaling
+# HELP NEEDED
 
+class HoneycombUnitCell:
+    def __init__(self,uc):      # Is there a way for me to just return a UnitCell object with out passing it in first?
+        self.__a = [np.array([1.,0.]),np.array([0.5,np.sqrt(3.)/2.])]
+        self.__b = [np.array([0.5,np.sqrt(3.)/6.]), np.array([1.,np.sqrt(3.)/3.])]
+        self.__dim = len(self.__a)
+        self.__spc = len(self.__b)
+        self.__uc = uc(self.__dim, self.__a, self.__spc, self.__b) # Not sure how to create a UnitCell object in this class
+
+    def create(self):           # I'm not sure hot to initialize and directly return an object
+        return self.__uc
+
+'''
 class UnitCellFactory:
-    def CreateUnitCell(self, UnitCellParam, Utype):
+    def CreateUnitCell(self, Utype):
         uc = self._GetUnitCell(Utype)
-        return uc(UnitCellParam)
+        return uc()
     
     def _GetUnitCell(self, Utype):
         if format == '1d':
@@ -22,29 +32,27 @@ class UnitCellFactory:
         else:
             raise ValueError(Utype)
 
-    def _Create2DHoneycomb(self, UnitCellParam, UnitCell):
+    def _Create2DHoneycomb(self, UnitCell):
         s = UnitCellParam.cell_scaling
-        a = s*np.array([[1.,0.5],
-                        [0.,np.sqrt(3)/2.]])
-        b = s*np.array([[0.5,          1.],
-                        [np.sqrt(3)/6.,np.sqrt(3)/3.]])
-        dim = np.size(a,1)
-        sites_per_cell = np.size(b,1)
+        a = s*[np.array([1.,0.]),np.array([0.5,np.sqrt(3.)/2.])]
+        b = s*[np.array([0.5,np.sqrt(3.)/6.]), np.array([1.,np.sqrt(3.)/3.])]
+        dim = len(a)
+        sites_per_cell = len(b)
         return UnitCell(dim, a, sites_per_cell, b)
 
-    def _Create1DUnitCell(self, UnitCellParam, UnitCell):
+    def _Create1DUnitCell(self, UnitCell):
         s = UnitCellParam.cell_scaling
-        a = s*np.array([[1.]])
-        b = s*np.array([[0.5]])
-        dim = np.size(a,1)
-        sites_per_cell = np.size(b,1)
+        a = s*[np.array([1.])]
+        b = s*[np.array([0.5])]
+        dim = len(a)
+        sites_per_cell = len(b)
         return UnitCell(dim, a, sites_per_cell, b)
 
-    def _Create2DSquareUnitCell(self, UnitCellParam, UnitCell):
+    def _Create2DSquareUnitCell(self, UnitCell):
         s = UnitCellParam.cell_scaling
-        a = s*np.array([[1.,0.],
-                        [0.,1.]])
-        b = s*np.array([[0.5,0.5]])
-        dim = np.size(a,1)
-        sites_per_cell = np.size(b,1)
+        a = s*[np.array([1.,0.]),np.array([0.,1.])]
+        b = s*[np.array([0.5,0.5])]
+        dim = len(a)
+        sites_per_cell = len(b)
         return UnitCell(dim, a, sites_per_cell, b)
+'''
