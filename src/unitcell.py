1c5cfb0244b0a8a213b4b8910bf8ef38ba5882af
Holt Sakai <Holt.Sakai@gmail.com>
2020-11-15 19:43:55 -0500

    todo notes from meeting

--- src/unitcell.py
+++ src/unitcell.py
@@ -1,11 +1,15 @@
 class UnitCell:
 
+    # TODO: only need a & b as inputs
+    # should also add checks b vectors have dim len(a)
     # unit cell attributes are private
     def __init__(self, dim, a, sites_per_cell, b):
         self.dim = dim
         self.a = a # array of unit vectors
         self.sites_per_cell = sites_per_cell
         self.b = b # array of site location(s) within cell
+
+        # TODO: add DOFs here
 
     # check equivalency of two unit cells
     def __eq__(self, other):
