ae3e52aca8e330fff46871bfea11647774bce310
lunlgit <lunl@princeton.edu>
2020-12-09 13:09:46 -0500

    Fixed unit cell unittest

--- test/test_unitcell.py
+++ test/test_unitcell.py
@@ -5,22 +5,22 @@ from unittest import TestCase
 class TestUnitCell(TestCase):
 
     def setUp(self):
-        self.uchc = HoneycombUnitCell(1)
-        self.ucsq = SquareUnitCell(1)
-        self.ucln = LineUnitCell(1)
+        self.uchc = HoneycombUnitCell((0,2))
+        self.ucsq = SquareUnitCell((2,))
+        self.ucln = LineUnitCell((2,))
 
     def test_honeycomb_unit_cell(self):
-        self.assertEqual(self.uchc.a, [np.array([1., 0.]),
-                                       np.array([0.5, np.sqrt(3.)/2.])])
-        self.assertEqual(self.uchc.b, [np.array([0.5, np.sqrt(3.)/6.]),
-                                       np.array([1., np.sqrt(3.)/3.])])
+        self.assertTrue(np.array_equal(self.uchc.a, [np.array([1., 0.]),
+                                                     np.array([0.5, np.sqrt(3.)/2.])]))
+        self.assertTrue(np.array_equal(self.uchc.b, [np.array([0.5, np.sqrt(3.)/6.]),
+                                                      np.array([1., np.sqrt(3.)/3.])]))
 
     def test_square_unit_cell(self):
-        self.assertEqual(self.ucsq.a, [np.array([1.,0.]),
-                                       np.array([0.,1.])])
-        self.assertEqual(self.ucsq.b, [np.array([0.5,0.5])])
+        self.assertTrue(np.array_equal(self.ucsq.a, [np.array([1.,0.]),
+                                                      np.array([0.,1.])]))
+        self.assertTrue(np.array_equal(self.ucsq.b, [np.array([0.5,0.5])]))
 
     def test_line_unit_cell(self):
-        self.assertEqual(self.ucln.a, [np.array([1.])])
-        self.assertEqual(self.ucln.b, [np.array([0.5])])
+        self.assertTrue(np.array_equal(self.ucln.a, [np.array([1.])]))
+        self.assertTrue(np.array_equal(self.ucln.b, [np.array([0.5])]))
 
